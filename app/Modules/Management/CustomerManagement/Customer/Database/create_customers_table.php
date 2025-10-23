cb809f04b0c8a603d16f202b410d90f7ae77f86a
tasnim-jawad <tasnimulh453@gmail.com>
2025-10-23 14:19:34 +0600

    new branch first commit from office

--- app/Modules/Management/CustomerManagement/Customer/Database/create_customers_table.php
+++ app/Modules/Management/CustomerManagement/Customer/Database/create_customers_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->enum('type', ['company', 'individual'])->nullable();
             $table->string('name', 100)->nullable();
             $table->string('phone', 20)->nullable();
             $table->string('email', 50)->nullable();
--- app/Modules/Management/SuppliyerManagement/Suppliyer/Database/create_suppliyers_table.php
+++ app/Modules/Management/SuppliyerManagement/Suppliyer/Database/create_suppliyers_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('suppliyers', function (Blueprint $table) {
             $table->id();
+            $table->enum('type', ['company', 'individual'])->nullable();
             $table->bigInteger('suppliyer_group_id')->nullable();
             $table->string('name', 100)->nullable();
             $table->string('phone', 20)->nullable();
